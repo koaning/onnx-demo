f01def03ae6d3a06c52c0c947e1981ac14131b87
Vincent <vincentwarmerdam@gmail.com>
2022-02-16 09:34:36 +0100

    onnx-update

--- train.py
+++ train.py
@@ -9,13 +9,13 @@ from sklearn.feature_extraction.text import CountVectorizer
 from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import StringTensorType
 
-
+# Load the training data
 df = pd.read_csv("clinc_oos-plus.csv").loc[lambda d: d['split'] == 'train']
 
 X = df['text'].to_list()
 y = df['label']
 
-
+# Make a very basic machine learning pipeline
 pipe = make_pipeline(
     make_union(
         CountVectorizer(),
@@ -24,10 +24,12 @@ pipe = make_pipeline(
 )
 
 pipe.fit(X, y)
+
+# Convert everything to the onnx format.
 initial_type = [('float_input', StringTensorType([None, 1]))]
 onx = convert_sklearn(pipe, initial_types=initial_type)
 
 with open("clinc-logreg.onnx", "wb") as f:
     f.write(onx.SerializeToString())
 
-dump(pipe, 'pipe.joblib') 
+dump(pipe, 'pipe.joblib')
